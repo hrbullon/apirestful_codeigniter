b6a9b6e467b9e79acb2fa6037b37529e8844b83d
Haderson Bullón <hrbullon@gmail.com>
2019-05-27 18:41:34 -0400

    Load JWT Library and Pretty Url

--- application/controllers/api/Test.php
+++ application/controllers/api/Test.php
@@ -8,38 +8,64 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 //To Solve File REST_Controller not found
 require APPPATH . 'libraries/REST_Controller.php';
 require APPPATH . 'libraries/Format.php';
- 
+require APPPATH . 'third_party/firebase/php-jwt/src/JWT.php';
+
+use Firebase\JWT\JWT;
+
 class Test extends REST_Controller {
  
     public function articles_get()
     {
-		$this->response([
-			'status' => TRUE,
-			'message' => 'respond with information about a user'
-		], REST_Controller::HTTP_OK);
+			$this->response([
+				'status' => TRUE,
+				'message' => 'respond with information about a user'
+			], REST_Controller::HTTP_OK);
     }
  
     public function articles_put()
     {
-        $this->response([
-			'status' => TRUE,
-			'message' => 'create a new user and respond with a status/errors'
-		], REST_Controller::HTTP_OK);
+			$this->response([
+				'status' => TRUE,
+				'message' => 'create a new user and respond with a status/errors'
+			], REST_Controller::HTTP_OK);
 	}
  
     public function articles_post()
     {
-		$this->response([
-			'status' => TRUE,
-			'message' => 'update an existing user and respond with a status/errors'
-		], REST_Controller::HTTP_OK);
+			$this->response([
+				'status' => TRUE,
+				'message' => 'update an existing user and respond with a status/errors'
+			], REST_Controller::HTTP_OK);
     }
  
     public function articles_delete()
     {
-		$this->response([
-			'status' => TRUE,
-			'message' => 'delete a user and respond with a status/errors'
-		], REST_Controller::HTTP_OK);
-    }
+			$this->response([
+				'status' => TRUE,
+				'message' => 'delete a user and respond with a status/errors'
+			], REST_Controller::HTTP_OK);
+		}
+		
+		public function load_library_get()
+		{
+				
+			$key = "example_key";
+			$token = array(
+					"iss" => "http://example.org",
+					"aud" => "http://example.com",
+					"iat" => 1356999524,
+					"nbf" => 1357000000,
+					"user" => "Haderson Bullon"
+			);
+
+			/**
+			 * IMPORTANT:
+			 * You must specify supported algorithms for your application. See
+			 * https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40
+			 * for a list of spec-compliant algorithms.
+			 */
+			$jwt = JWT::encode($token, $key);
+			$decoded = JWT::decode($jwt, $key, array('HS256'));
+			echo var_dump($decoded);			
+		}
 }
